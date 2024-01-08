46ce1eebf8ccf7df4454a4f9aeab9fc74ff1e5d8
LW95x <liam.woodall@live.co.uk>
2024-01-08 14:49:00 +0000

    str method changed

--- treasure_hunt_backend/models.py
+++ treasure_hunt_backend/models.py
@@ -18,7 +18,7 @@ class Profile(models.Model):
     treasures = models.ManyToManyField('Treasure', related_name='owners', blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user_id.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
