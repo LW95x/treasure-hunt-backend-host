1d752217e0fadbf4f76dcbdb09625b26c2d69aee
LW95x <liam.woodall@live.co.uk>
2024-01-09 17:09:29 +0000

    re-enabled profile prevention and deletion

--- treasure_hunt_backend/models.py
+++ treasure_hunt_backend/models.py
@@ -14,7 +14,7 @@ class Treasure(models.Model):
     
     
 class Profile(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     treasures = models.ManyToManyField('Treasure', related_name='owners', blank=True)
 
     @property
